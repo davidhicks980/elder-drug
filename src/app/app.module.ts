3e632c59c01526c308ccebca929de0df8084117e
David Hicks <davidhicks980@gmail.com>
2021-10-10 14:01:31 -0400

    removed extraneous components from app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MAT_RIPPLE_GLOBAL_OPTIONS, RippleGlobalOptions } from '@angular/material/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
@@ -16,6 +15,7 @@ import { AppComponent } from './app.component';
 import { BrandComponent } from './components/brand/brand.component';
 import { VerticalComponent } from './components/brand/vertical/vertical.component';
 import { ErrorMessageComponent } from './components/error-message/error-message.component';
+import { GradientIconComponent } from './components/icon/icon.component';
 import { LayoutComponent } from './components/layout/layout.component';
 import { AnimatedArrowComponent } from './components/layout/menu-toggle/animated-arrow/animated-arrow.component';
 import { AnimatedXComponent } from './components/layout/menu-toggle/animated-x/animated-x.component';
@@ -30,14 +30,15 @@ import {
 } from './components/layout/side-navigation/drug-form/typeahead/autocomplete-content.component';
 import { SidebarBrandDirective, SidebarToggleDirective } from './components/layout/side-navigation/sidebar-brand.directive';
 import { SidebarComponent } from './components/layout/side-navigation/sidebar.component';
-import { TabLinkComponent } from './components/layout/tab/tab-link/tab-link.component';
-import { TabComponent } from './components/layout/tab/tab.component';
 import {
   AboutComponent,
   DesignComponent,
   DisclaimerComponent,
   ToolbarComponent,
 } from './components/layout/top-toolbar/toolbar.component';
+import { TabLinkComponent } from './components/tab/tab-link/tab-link.component';
+import { TabComponent } from './components/tab/tab.component';
+import { CellComponent } from './components/table/cell/cell.component';
 import { ExpandToggleDirective } from './components/table/directives/expand-toggle.directive';
 import { ColumnSelectorComponent } from './components/table/dropdown/column-selector/column-selector.component';
 import { DropItemComponent } from './components/table/dropdown/group-by/button/drop-item.component';
@@ -45,30 +46,29 @@ import { GroupByComponent } from './components/table/dropdown/group-by/group-by.
 import { PopupContentDirective } from './components/table/dropdown/popup-content.directive';
 import { PopupContentComponent } from './components/table/dropdown/popup-content/popup-placeholder.component';
 import { PopupComponent } from './components/table/dropdown/popup.component';
+import { EntryRangeCardComponent } from './components/table/entry-range-card/entry-range-card.component';
 import { ExpandedElementComponent } from './components/table/expanded-element/expanded-element.component';
-import { ExpandedRowCardComponent } from './components/table/expanded-element/expanded-row-card/expanded-row-card.component';
+import { ExpandedRowCardComponent } from './components/table/expanded-row-card/entry-card.component';
 import { FilterInputComponent } from './components/table/filter-input/filter-input.component';
-import { FilterComponent } from './components/table/filter/filter.component';
 import { GroupRowComponent } from './components/table/group-row/group-row.component';
 import { HighlightPipe } from './components/table/highlight.pipe';
-import { CellComponent } from './components/table/row/cell.component';
 import { SafeHtmlPipe } from './components/table/safe-html.pipe';
 import { TableCardComponent } from './components/table/table-card/table-card.component';
+import { FilterBarComponent } from './components/table/table-filter-bar/filter-bar.component';
 import { TableComponent } from './components/table/table.component';
-import { FilterBarComponent } from './components/table/toolbar/filter-bar.component';
 import { TextIconButtonComponent } from './components/text-icon-button/text-icon-button.component';
-import { ExpansionPanelComponent } from './components/unused/expansion-panel/expansion-panel.component';
 import { AutoFocusDirective } from './directives/auto-focus.directive';
 import { CellPaddingDirective } from './directives/cell-padding.directive';
 import { TemplateContentDirective } from './directives/content-template.directive';
 import { CreationSpyDirective } from './directives/creation-spy.directive';
 import { ExpandableRowDirective } from './directives/expandable-row.directive';
-import { FilterDirective } from './directives/filter.directive';
 import { IconButtonDirective } from './directives/icon-button.directive';
 import { KeyGridDirective } from './directives/keygrid.directive';
 import { ListKeyDirective } from './directives/list-key.directive';
-import { ResizeDirective } from './directives/resize.directive';
+import { RippleDirective } from './directives/ripple.directive';
 import { RotateDirective } from './directives/rotate-icon.directive';
+import { RowToggleDirective } from './directives/row-toggle.directive';
+import { SvgGradientDirective } from './directives/svg-gradient.directive';
 import { TrackingGradientDirective } from './directives/tracking-gradient.directive';
 import { LetDirective } from './directives/with.directive';
 import { BEERS_ENTRIES, beersEntries } from './injectables/brand-drugs.injectable';
@@ -84,13 +84,6 @@ import { AddComponent } from './svg/add/add.component';
 
 export const firebase = provideFirebaseApp(() => initializeApp(environment.firebaseConfig));
 export const firestore = provideFirestore(() => getFirestore());
-const globalRippleConfig: RippleGlobalOptions = {
-  disabled: false,
-  animation: {
-    enterDuration: 400,
-    exitDuration: 100,
-  },
-};
 
 @NgModule({
   declarations: [
@@ -104,7 +97,6 @@ const globalRippleConfig: RippleGlobalOptions = {
     DisclaimerComponent,
     AboutComponent,
     DesignComponent,
-    ExpansionPanelComponent,
     ExpandedElementComponent,
     RotateDirective,
     TrackingGradientDirective,
@@ -112,7 +104,6 @@ const globalRippleConfig: RippleGlobalOptions = {
     TabComponent,
     ColumnSelectorComponent,
     PopupComponent,
-    ResizeDirective,
     GroupByComponent,
     JoinPipe,
     ListKeyDirective,
@@ -121,8 +112,6 @@ const globalRippleConfig: RippleGlobalOptions = {
     LetDirective,
     CellPaddingDirective,
     KeyGridDirective,
-    FilterComponent,
-    FilterDirective,
     TableCardComponent,
     ExpandableRowDirective,
     GroupRowComponent,
@@ -152,6 +141,11 @@ const globalRippleConfig: RippleGlobalOptions = {
     HighlightPipe,
     SafeHtmlPipe,
     TableComponent,
+    RowToggleDirective,
+    EntryRangeCardComponent,
+    RippleDirective,
+    SvgGradientDirective,
+    GradientIconComponent,
   ],
   imports: [
     firebase,
@@ -175,9 +169,9 @@ const globalRippleConfig: RippleGlobalOptions = {
     { provide: TABLE_CONFIG, useValue: tableConfig },
     { provide: GENERIC_DRUGS, useValue: genericDrugNames },
     { provide: BEERS_ENTRIES, useValue: beersEntries },
-    { provide: MAT_RIPPLE_GLOBAL_OPTIONS, useValue: globalRippleConfig },
   ],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  exports: [GradientIconComponent],
 })
 export class AppModule {}
