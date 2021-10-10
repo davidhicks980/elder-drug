0bbaf9b71eb8797d95ad62c956a13e605ac69ff9
David Hicks <davidhicks980@gmail.com>
2021-10-10 14:26:33 -0400

    Added gradient icons using icon registry and made table header more pronounced

--- src/app/components/table/table-card/table-card.component.ts
+++ src/app/components/table/table-card/table-card.component.ts
@@ -1,5 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
-import { combineLatest } from 'rxjs';
+import { Component, Input } from '@angular/core';
 
 import { ColumnService } from '../../../services/columns.service';
 import { FilterService } from '../../../services/filter.service';
@@ -13,45 +12,11 @@ import { TableService } from '../../../services/table.service';
   providers: [GroupByService, ColumnService, FilterService],
 })
 export class TableCardComponent {
-  @Output() columns: EventEmitter<string[]> = new EventEmitter();
-  @Output() groups: EventEmitter<string[]> = new EventEmitter();
-  activeTable: number;
-  formatGroupNames(name: string): string {
-    return name?.replace(/([A-Z])/g, ' $1').trim() || '';
-  }
+  @Input() icon: string;
   constructor(
     private columnService: ColumnService,
     private groupService: GroupByService,
     private tableService: TableService,
     private filterService: FilterService
-  ) {
-    combineLatest([
-      this.groupService.groupedItems$,
-      this.groupService.ungroupedItems$,
-      this.columnService.selected$,
-      this.columnService.columns$,
-    ]).subscribe(([groupedFields, ungroupedFields, selectedColumns, columns]) => {
-      this.tableService.updateTableState(
-        {
-          groupedFields,
-          ungroupedFields,
-          selectedColumns,
-          columns,
-        },
-        this.activeTable
-      );
-    });
-    this.tableService.selection$.subscribe((table) => {
-      this.activeTable = table.tableNumber;
-      this.columnService.changeTable(table);
-      let state = this.tableService.activeTableState;
-      if (state) {
-        let { groupedFields: grouped, ungroupedFields: ungrouped, selectedColumns } = state;
-        this.columnService.emitColumns(selectedColumns);
-        this.groupService.addItems({ ungrouped, grouped }, true);
-        this.columns.emit(selectedColumns);
-        this.groups.emit(grouped);
-      }
-    });
-  }
+  ) {}
 }
---
+++ src/app/directives/svg-gradient.directive.ts
@@ -0,0 +1,85 @@
+import { Directive, ElementRef, HostBinding, Input, Renderer2, ViewChild } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { take } from 'rxjs/operators';
+
+@Directive({
+  selector: '[svgGradient]',
+})
+export class SvgGradientDirective {
+  private _visible: string = '';
+  fill: string = '';
+  @ViewChild(SvgGradientDirective) children;
+  refresh: boolean;
+  svg: Promise<SVGElement>;
+  @HostBinding('style.--svg-fill')
+  get svgFill() {
+    return this.fill;
+  }
+  @Input('svgGradient')
+  gradient: Record<string, string> = { '0': '#fff', '1': '#000' };
+  @Input('svgGradientType') type: 'linear' | 'radial' = 'linear';
+  definition: SVGDefsElement;
+  @Input('svgGradientAngle') angle: number = 0;
+  @Input('svgGradientName')
+  get name(): string {
+    return this._visible;
+  }
+  set name(value: string) {
+    if (value != this._visible) {
+      this._visible = value;
+      this.registry
+        .getNamedSvgIcon(value)
+        .pipe(take(1))
+        .subscribe((icon: SVGSVGElement) => {
+          this.appendSVG(icon);
+        });
+    }
+  }
+  appendSVG(icon: SVGSVGElement) {
+    this.renderer.selectRootElement(this.element.nativeElement);
+    this.gradientId = this.generateGradientId();
+    let def = this.createGradientDefinition(this.gradientId);
+    this.renderer.appendChild(icon, def);
+    this.fill = `url(#${this.gradientId})`;
+    this.renderer.appendChild(this.element.nativeElement, icon);
+  }
+
+  gradientId: string;
+  svgGradient: SVGGradientElement;
+
+  constructor(
+    private element: ElementRef,
+    private renderer: Renderer2,
+    private registry: MatIconRegistry
+  ) {}
+
+  private generateGradientId() {
+    return String(Math.round(Math.random() * 10 ** 6)) + String(Date.now());
+  }
+  private createSVG(name: string) {
+    return this.renderer.createElement(name, 'svg');
+  }
+  private setSVGAttribute(element: SVGElement, attribute: string, value: string) {
+    this.renderer.setAttribute(element, attribute, value);
+    return (nextAttribute, nextValue) => this.setSVGAttribute(element, nextAttribute, nextValue);
+  }
+
+  private createGradientDefinition(id: string) {
+    const svgGradient = this.createSVG(`${this.type}Gradient`) as SVGGradientElement;
+    let definition = this.createSVG('defs') as SVGDefsElement;
+    this.setSVGAttribute(svgGradient, 'id', id)('gradientTransform', `rotate(${this.angle})`);
+    this.createGradientStops(this.gradient).forEach((stop) => {
+      this.renderer.appendChild(svgGradient, stop);
+    });
+    this.renderer.appendChild(definition, svgGradient);
+    return definition;
+  }
+
+  private createGradientStops(gradient: Record<number, string>) {
+    return Object.entries(gradient).map(([offset, color]) => {
+      let stop = this.createSVG('stop');
+      this.setSVGAttribute(stop, 'stop-color', color)('offset', Number(offset));
+      return stop;
+    });
+  }
+}
